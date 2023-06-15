a95c175da75a19c01ff5961356291f6c10f5aec1
Kamensky Andrey <kamensky24@gmail.com>
2023-06-15 23:30:57 +0700

    16 validate

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -9,24 +9,50 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {useFormik} from "formik";
 
-const formik = useFormik({
-    initialValues: {
-        email: '',
-        password: '',
-        rememberMe: false
-    },
-    onSubmit: values => {
-        alert(JSON.stringify(values));
-    },
-})
-
-
 export const Login = () => {
+
+    type FormikErrorType = {
+        email?: string
+        password?: string
+        rememberMe?: boolean
+    }
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate:(values)=>{
+          const errors: FormikErrorType = {};
+          if (!values.email) {
+              errors.email='Required';
+          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+              errors.email = 'Invalid email address';
+          }
+
+            if (!values.password) {
+                errors.password='Required';
+            } else if (values.password.length < 3) {
+                errors.password = 'Length must be more 3 symbols';
+            }
+
+          return errors
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+        },
+    })
+
+    // console.log(formik.values)
+
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
-            <form onSubmit={formik.handleSubmit}>
-                <FormControl>
+
+            <FormControl>
+                <form onSubmit={formik.handleSubmit}>
                     <FormLabel>
+
                         <p>To log in get registered
                             <a href={'https://social-network.samuraijs.com/'}
                                target={'_blank'}> here
@@ -42,15 +68,19 @@ export const Login = () => {
                             margin="normal"
                             name='email'
                             onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                             value={formik.values.email}/>
+                        {formik.touched.email && formik.errors.email && <div style={{color:'red'}}>{formik.errors.email}</div>}
+                        {/*{formik.errors.email ? <div style={{color:'red'}}>{formik.errors.email}</div>:null}*/}
                         <TextField
                             type="password"
                             label="Password"
                             margin="normal"
                             name='password'
                             onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                             value={formik.values.password}/>
-
+                        {formik.touched.password && formik.errors.password && <div style={{color:'red'}}>{formik.errors.password}</div>}
                         <FormControlLabel label={'Remember me'}
                                           control={<Checkbox onChange={formik.handleChange}
                                                              checked={formik.values.rememberMe}
@@ -59,8 +89,9 @@ export const Login = () => {
                             Login
                         </Button>
                     </FormGroup>
-                </FormControl>
-            </form>
+                </form>
+            </FormControl>
+
         </Grid>
     </Grid>
 }
